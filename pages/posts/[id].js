23f7a04adbb71c4d6f8d011eaf7b15c70b953d8f
telemacowill <aglwill@gmail.com>
2023-12-22 15:11:40 -0300

    render markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,8 @@ import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id);
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
   return {
     props: {
       postData,
@@ -17,6 +18,17 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Post() {
-  return <Layout>...</Layout>;
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{__html: postData.contentHtml }} />
+    </Layout>
+  );
 }
+
