63e4518d9fd7cfc95bf732557ef3bdfdf2266da5
telemacowill <aglwill@gmail.com>
2023-12-28 11:28:21 -0300

    Polishing the Index Page - and contentHtml fix

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -7,8 +7,6 @@ import utilStyles from '../../styles/utils.module.css';
 export async function getStaticProps({ params }) {
   // Add the "await" keyword like this:
   const postData = await getPostData(params.id);
-
-
   return {
     props: {
       postData,
